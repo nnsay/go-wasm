8d076f149b31209510e392e4753b137284e77c89
Jimmy Wang <jimmy.w@aliyun.com>
2024-09-09 19:04:20 +0800

    chore: update latest tinygo wasm

--- tinygo-getting-started/setup.sh
+++ tinygo-getting-started/setup.sh
@@ -4,7 +4,8 @@ echo "1.拷贝wasm_exec.js"
 cp "$(tinygo env TINYGOROOT)/targets/wasm_exec.js" tinygo-getting-started/
 
 echo "2.编译wasm"
-tinygo build -o tinygo-getting-started/main.wasm -target wasm ./tinygo-getting-started
+GOOS=js GOARCH=wasm tinygo build -o tinygo-getting-started/main.wasm -target wasm ./tinygo-getting-started
+# tinygo build -o tinygo-getting-started/main.wasm -target wasm ./tinygo-getting-started
 
 echo "3.开启静态网站"
 caddy file-server --listen :3000 --root tinygo-getting-started
